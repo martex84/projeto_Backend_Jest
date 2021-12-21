6ec7a25eae3bedd95567078edbab21483705efce
martex84 <martex849@gmail.com>
2021-12-21 20:03:17 -0300

    build: Inclusao dependencia Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+export default {
+  "host": "127.0.0.1",
+  "username": "postgres",
+  "password": "root",
+  "database": "nodeauth",
+  "dialect": "postgres",
+  "operatorsAlises": false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
