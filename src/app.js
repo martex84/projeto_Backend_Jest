c8846dbff0dc533e4d0cd268a372ea8a4f88e20d
martex84 <martex849@gmail.com>
2021-12-21 18:54:32 -0300

    feat: Inclusao do arquivo App para Express

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import routes from './routes.js'
+
+class AppControler {
+    constructor() {
+        this.express = express();
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.express.use(express.json());
+    }
+
+    routes() {
+        this.express.use(routes);
+    }
+
+}
+
+const appControler_express = new AppControler().express;
+
+export {
+    appControler_express
+}
